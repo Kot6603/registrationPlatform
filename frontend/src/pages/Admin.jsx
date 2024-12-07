6716a0fdf2d1f18c85bf0ccb360ad27aad4e7fc2
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-07 20:46:08 +1300

    :construction: admin page wip

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,6 +1,15 @@
+import EventContainer from '../components/EventContainer'
+
 function Admin() {
   return (
-    <h1>Admin</h1>
+    <div>
+      <header className="p-4 bg-gray-800 shadow-lg rounded-md m-4">
+        <h1 className="text-2xl font-bold text-white">Admin</h1>
+      </header>
+      <div className="max-w-screen">
+        <EventContainer events={[]} />
+      </div>
+    </div>
   )
 }
 
