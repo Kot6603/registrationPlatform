8b3119f0c506b973836ea5c954b4e53bf3310ea7
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-07 17:40:25 +1300

    :sparkles: implemented login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,59 @@
+import { Link } from "react-router"
+
 function Login() {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password)
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-gray-800 p-10 rounded-md shadow-lg w-full max-w-lg">
+        <h2 className="text-2xl text-white font-bold mb-6 text-center">Login</h2>
+        <form className="space-y-4" onSubmit={handleLogin}>
+          <div>
+            <label htmlFor="email" className="text-white block">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              className="w-full p-3 mt-1 bg-gray-700 text-white border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="text-white block">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              className="w-full p-3 mt-1 bg-gray-700 text-white border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+            />
+          </div>
+          <div className="mt-4 text-center">
+            <p className="text-white">
+              Don't have an account?{" "}
+              <Link
+                to="/signup"
+                className="text-blue-400 hover:text-blue-500"
+              >
+                Sign up here
+              </Link>
+            </p>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-white text-black py-3 rounded-md hover:bg-gray-100 transition duration-200"
+          >
+            Log In
+          </button>
+        </form>
+      </div >
+    </div >
+  );
 }
 
-export default Login
+export default Login;
