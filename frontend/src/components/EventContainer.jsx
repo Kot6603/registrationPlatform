dca991c25b2cd459f17aadf2cc2c5e1ab19c2940
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-08 02:14:22 +1300

    :lipstick: style revamp on the landing page

--- frontend/src/components/EventContainer.jsx
+++ frontend/src/components/EventContainer.jsx
@@ -3,9 +3,15 @@ import EventCard from "./EventCard"
 function EventContainer({ events }) {
   return (
     <div className="bg-white p-5 rounded-lg">
+      <h2 className="text-3xl font-bold mb-4">Events</h2>
       {events.map((event, _) => {
         return (
-          <EventCard key={event.name + event.date} name={event.name} date={event.date} desc={event.description} />
+          <EventCard
+            key={event.name + event.date}
+            name={event.name}
+            date={new Date(event.date).toDateString()}
+            desc={event.description}
+          />
         )
       })}
     </div >
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,27 +1,54 @@
 import EventContainer from '../components/EventContainer'
-import Header from '../components/Header'
+import { useContext, useEffect } from "react"
+import EventContext from '../context/EventContext'
+import EventService from "../services/event"
 
 function LandingPage() {
-  const events = [
-    {
-      "name": "event 1",
-      "date": "tuesday",
-      "description": "desc of event 1"
-    },
-    {
-      "name": "event 2",
-      "date": "wednesday",
-      "description": "desc of event 2"
-    },
-  ]
+  const { events, setEvents } = useContext(EventContext)
+
+  useEffect(() => {
+    EventService
+      .getAll()
+      .then(notes => {
+        setEvents(notes)
+      })
+  }, [])
+
+  const handleSignup = () => {
+    console.log("Signing up")
+  }
+
+  const handleLogin = () => {
+    console.log("Logging in")
+  }
+
   return (
     <div>
-      <Header
-        heading={"Landing Page"}
-        onClick={() => console.log("hi from landing")}
-        buttonText={"Log In"}
-      />
-      <EventContainer events={events} />
+      <header className="p-4 bg-gray-800 shadow-lg rounded-md m-4">
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold text-white">Event Registery</h1>
+          <div>
+            <button
+              onClick={handleSignup}
+              className="bg-white text-black mx-2 px-4 py-2 rounded-md shadow-md hover:bg-gray-100 transition duration-200"
+            >
+              Sign Up
+            </button>
+            <button
+              onClick={handleLogin}
+              className="bg-white text-black mx-2 px-4 py-2 rounded-md shadow-md hover:bg-gray-100 transition duration-200"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </header>
+      <div className="m-8">
+        <div className="w-full mx-auto p-4 border-2 border-white bg-red-400 shadow-lg rounded-md text-center my-6">
+          <p className="text-white">You are not logged in. Sign Up / Login to register for an event.</p>
+        </div >
+        <EventContainer events={events} />
+      </div>
     </div>
   )
 }
