9a00de3e8b3984c8f9eb0c9ca863f48d10b91689
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-07 17:54:56 +1300

    :lipstick: made styling consistent on the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="bg-gray-800 p-10 rounded-md shadow-lg w-full max-w-lg">
-        <h2 className="text-2xl text-white font-bold mb-6 text-center">Login</h2>
+        <h2 className="text-3xl text-white font-bold mb-6 text-center">Login</h2>
         <form className="space-y-4" onSubmit={handleLogin}>
           <div>
             <label htmlFor="email" className="text-white block">Email</label>
@@ -33,24 +33,26 @@ function Login() {
               className="w-full p-3 mt-1 bg-gray-700 text-white border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-white"
             />
           </div>
-          <div className="mt-4 text-center">
-            <p className="text-white">
-              Don't have an account?{" "}
-              <Link
-                to="/signup"
-                className="text-blue-400 hover:text-blue-500"
-              >
-                Sign up here
-              </Link>
-            </p>
+          <div className="w-full text-center">
+            <button
+              type="submit"
+              className="mt-2 w-1/2 bg-white text-black py-3 rounded-md hover:bg-gray-100 transition duration-200"
+            >
+              Log In
+            </button>
           </div>
-          <button
-            type="submit"
-            className="w-full bg-white text-black py-3 rounded-md hover:bg-gray-100 transition duration-200"
-          >
-            Log In
-          </button>
         </form>
+        <div className="mt-4 text-center">
+          <p className="text-white">
+            Don't have an account?{" "}
+            <Link
+              to="/signup"
+              className="text-blue-400 hover:text-blue-500"
+            >
+              Sign up here
+            </Link>
+          </p>
+        </div>
       </div >
     </div >
   );
