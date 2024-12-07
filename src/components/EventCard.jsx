22e3e2e8ed7624e36a81e9c4a104a96160bf7e48
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-07 14:34:23 +1300

    :sparkles: implemented event card component to display event info

---
+++ src/components/EventCard.jsx
@@ -0,0 +1,13 @@
+function EventCard({ name, date, desc }) {
+  return (
+    <div className="w-4/5 max-w-screen-lg mx-auto p-4 border-2 border-white bg-black shadow-lg rounded-md m-2">
+      <div className="flex justify-between items-center border-b-2 border-white pb-2 mb-2">
+        <h2 className="text-xl font-bold text-white">{name}</h2>
+        <h3 className="text-sm text-gray-200">{date}</h3>
+      </div>
+      <p className="text-white">{desc}</p>
+    </div >
+  )
+}
+
+export default EventCard
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,11 @@
+import EventCard from '../components/EventCard'
+
 function LandingPage() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="red">
+      <h1>Landing Page</h1>
+      <EventCard name={"event 1"} date={"tuesday"} desc={"some desc"} />
+      <EventCard name={"event 2"} date={"wednesday"} desc={"some desc"} />
     </div>
   )
 }
