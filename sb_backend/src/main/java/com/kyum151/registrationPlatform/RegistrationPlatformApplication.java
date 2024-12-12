8e7698fad6f87509bca84dc4f116fbf564944e53
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-12 23:10:40 +1300

    :sparkles: initial commit to spring boot backend

---
+++ sb_backend/src/main/java/com/kyum151/registrationPlatform/RegistrationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kyum151.registrationPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationPlatformApplication.class, args);
+	}
+
+}
---
+++ sb_backend/src/test/java/com/kyum151/registrationPlatform/RegistrationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyum151.registrationPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
