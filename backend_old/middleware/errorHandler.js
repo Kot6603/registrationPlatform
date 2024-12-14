0e744eeb81e010bfc3d3bda5126e35beda87aacf
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-15 11:20:23 +1300

    :coffin: removed the old backend code

--- backend_old/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function errorHandler(error, _, response, next) {
-  console.error(error.message);
-  next(error);
-}
-
-export default errorHandler;
--- backend_old/middleware/unknownEndpoint.js
+++
@@ -1,5 +0,0 @@
-function unknownEndpoint(_, response) {
-  response.status(404).send({ error: "unknown endpoint" });
-}
-
-export default unknownEndpoint;
