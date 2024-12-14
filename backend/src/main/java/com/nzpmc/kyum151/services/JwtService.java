9702b7c24992a6e2410cfd0da9fdd39720b399ae
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-15 12:54:48 +1300

    :sparkles: implemented jwt service

--- backend/src/main/java/com/nzpmc/kyum151/services/JwtService.java
+++ backend/src/main/java/com/nzpmc/kyum151/services/JwtService.java
@@ -1,33 +1,87 @@
 package com.nzpmc.kyum151.services;
 
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
-import org.springframework.stereotype.Component;
-
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
-@Component
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtService {
-  private final JwtEncoder jwtEncoder;
+  @Value("${security.jwt.secret-key}")
+  private String SECRET_KEY;
 
-  public JwtService(JwtEncoder jwtEncoder) {
-    this.jwtEncoder = jwtEncoder;
+  @Value("${security.jwt.expiration-time}")
+  private long EXPIRATION_TIME;
+
+  public String extractUsername(String token) {
+    return extractClaim(token, Claims::getSubject);
   }
 
-  public String encode(Map<String, Object> claims) {
-    Instant now = Instant.now();
-    Instant expiry = now.plus(3, ChronoUnit.DAYS);
-    JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
-        .issuer("kyum151")
-        .subject("user")
-        .issuedAt(now)
-        .expiresAt(expiry)
-        .claims(claims)
-        .build();
-    JwtEncoderParameters parameters = new JwtEncoderParameters(jwtClaimsSet);
-    return this.jwtEncoder.encode(parameters).getTokenValue();
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
+
+  public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+    return buildToken(extraClaims, userDetails, EXPIRATION_TIME);
+  }
+
+  public long getExpirationTime() {
+    return EXPIRATION_TIME;
+  }
+
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private String buildToken(
+      Map<String, Object> extraClaims,
+      UserDetails userDetails,
+      long EXPIRATION_TIME) {
+    return Jwts
+        .builder()
+        .setClaims(extraClaims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+        .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+        .compact();
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts
+        .parserBuilder()
+        .setSigningKey(getSignInKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
+
+  private Key getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBytes);
   }
 }
