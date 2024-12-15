471007dc08b755427c0e033c768a3bb5d46b6d53
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-16 00:47:46 +1300

    :sparkles: better error logging and proper error http response

---
+++ backend/src/main/java/com/nzpmc/kyum151/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.nzpmc.kyum151.exceptions;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(Exception.class)
+  public ProblemDetail handleSecurityException(Exception exception) {
+    ProblemDetail errorDetail = null;
+
+    exception.printStackTrace();
+
+    if (exception instanceof BadCredentialsException) {
+      errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
+      errorDetail.setProperty("message", "Invalid username or password");
+
+      return errorDetail;
+    }
+    if (exception instanceof AccountStatusException) {
+      errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("message", "The account is locked");
+    }
+    if (exception instanceof AccessDeniedException) {
+      errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("message", "You are not authorised to access this resource");
+    }
+    if (exception instanceof SignatureException) {
+      errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("message", "The JWT signature is invalid");
+    }
+    if (exception instanceof ExpiredJwtException) {
+      errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("message", "The JWT token has expired");
+    }
+    if (errorDetail == null) {
+      errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
+      errorDetail.setProperty("message", "Unknown internal server error");
+    }
+    return errorDetail;
+  }
+}
