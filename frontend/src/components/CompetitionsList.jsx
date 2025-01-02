2b1f1184796e45f9dbe6c3f7236937a0fa82d8a2
Kot <89110272+Kot6603@users.noreply.github.com>
2025-01-02 23:52:27 +1300

    :lipstick: styled sidebar to fit on one page

--- frontend/src/components/CompetitionsList.jsx
+++ frontend/src/components/CompetitionsList.jsx
@@ -16,24 +16,29 @@ function CompetitionsList({ competitions, activeCompetition, setActiveCompetitio
           className="border-2 border-gray-300 rounded-md p-1 text-sm font-normal"
         />
       </div>
-      {competitionsToShow.map((comp) => {
-        return (
-          <div
-            key={comp.title}
-            className="flex items-center space-x-4"
-          >
-            <button
-              className={`w-full mx-auto p-2 border-2 border-white ${comp.id === activeCompetition?.id ? "bg-blue-700" : "bg-gray-800"} shadow-lg rounded-md m-1`}
-              onClick={() => setActiveCompetition({
-                id: comp.id,
-                title: comp.title
-              })}
+      <div
+        className="max-h-[150px] overflow-y-auto"
+        style={{ scrollbarWidth: "none" }}
+      >
+        {competitionsToShow.map((comp) => {
+          return (
+            <div
+              key={comp.title}
+              className="flex items-center space-x-4"
             >
-              <p className="text-lg font-bold text-white">{comp.title}</p>
-            </button >
-          </div>
-        )
-      })}
+              <button
+                className={`w-full mx-auto p-2 border-2 border-white ${comp.id === activeCompetition?.id ? "bg-blue-700" : "bg-gray-800"} shadow-lg rounded-md m-1`}
+                onClick={() => setActiveCompetition({
+                  id: comp.id,
+                  title: comp.title
+                })}
+              >
+                <p className="text-lg font-bold text-white">{comp.title}</p>
+              </button >
+            </div>
+          )
+        })}
+      </div>
     </div >
   );
 }
